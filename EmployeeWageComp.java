33d28b736aef2525dd880b1be4824e15c369b664
santro93 <nsantosh435@gmail.com>
2022-03-10 23:45:57 +0530

    Employee Wage Calculation as per Present, Absent Or Half Time  By using Switch

--- EmployeeWageComp.java
+++ EmployeeWageComp.java
@@ -9,34 +9,34 @@ public class EmployeeWageComp {
 		public static final int WAGE_PER_HR		= 20;
 		public static final int FULL_DAY_HR		= 8;
 		public static final int HALF_DAY_HR 	= 4;
-		
-		
-	public static void main(String[] args) {
-	    // Print Program Name
+		int dailyWage = 0;
+		public static void main(String[] args) {
+								// Print Program Name
 		           System.out.println("\nWelcome to Employee Wage Computation Program");
 		          
-		//Check Employee is Present , Absent Or Half Time  By using Random 
-		           //Employee is Present
-				if (IS_PRESENT == RANDOM ) {
-					System.out.println("Employee is Present");
-					// Calculate Daily Employee Wage
-					int dailyWage = WAGE_PER_HR * FULL_DAY_HR;
-					System.out.println(dailyWage);
-				}
-				 //Employee is Absent
-				else if (ABSENT== RANDOM ){
-					System.out.println("Employee is Absent");
-					// Calculate Daily Employee Wage
-					int dailyWage = 0;
-					System.out.println(dailyWage);
-				}
-				 //Employee is Half Time
-				else if (IS_HALF_PRESENT == RANDOM ) {
-					System.out.println("Employee is Half Time");
-					// Calculate Daily Employee Wage
-					int halfdailyWage = WAGE_PER_HR * HALF_DAY_HR;
-					System.out.println(halfdailyWage);
-		
+		           				//Check Employee is Present , Absent Or Half Time  By using Random 
+		           switch((int)RANDOM){
+		           						//Employee is Absent
+		           		case 0:     	
+		           							System.out.println("Employee is Absent");
+	        	           		        // Calculate Daily Employee Wage
+	        	           		   			int dailyWage  = WAGE_PER_HR * ABSENT;
+	        	           		   		   System.out.println(dailyWage);     
+	        	           		   				        break;
+	        	           		   			//Employee is Present
+		           		case 1:			   
+		           							System.out.println("Employee is Present");
+		           						// Calculate Daily Employee Wage
+		           							dailyWage = WAGE_PER_HR * FULL_DAY_HR;
+		           						   System.out.println(dailyWage);
+					   									break;
+						        	   	//Employee is Half Time
+		           		case 2:
+		           							System.out.println("Employee is Half Time");
+									   // Calculate Daily Employee Wage
+						        	       int halfdailyWage = WAGE_PER_HR * HALF_DAY_HR;
+						        	   	   System.out.println(halfdailyWage);
+						
 				}
 	
 				
