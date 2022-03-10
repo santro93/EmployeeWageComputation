19a89c0d2a21a021f3713962b6c0f824ed67338c
santro93 <nsantosh435@gmail.com>
2022-03-10 17:32:29 +0530

    Verify Employee is Present or Absent

--- EmployeeWageComp.java
+++ EmployeeWageComp.java
@@ -1,11 +1,25 @@
 package employeeWageComp;
 
 public class EmployeeWageComp {
-	   
+	// Declared global variables
+		public static double RANDOM = Math.floor(Math.random()*2);
+		public static final int IS_PRESENT = 1;
+			
 
 	public static void main(String[] args) {
 	   // Print Program Name
-		System.out.println("\n Welcome to Employee Wage Computation Program");
+		System.out.println("\nWelcome to Employee Wage Computation Program");
+	
+		//Check Employee is Present or Absent By using Random 
+		
+				if (IS_PRESENT == RANDOM ) {
+					System.out.println("Employee is Present");
+				}
+				else{
+					System.out.println("Employee is Absent");
+				}
+	
+	
 	}
 
 }
