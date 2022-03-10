068e669654118d7af67d6bfcd0fa25c4cef84e24
santro93 <nsantosh435@gmail.com>
2022-03-10 17:28:36 +0530

    Welcome to Employee Wage Computation Program

---
+++ EmployeeWageComp.java
@@ -0,0 +1,11 @@
+package employeeWageComp;
+
+public class EmployeeWageComp {
+	   
+
+	public static void main(String[] args) {
+	   // Print Program Name
+		System.out.println("\n Welcome to Employee Wage Computation Program");
+	}
+
+}
