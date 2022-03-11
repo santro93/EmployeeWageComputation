a321e3a195a76a6e499ad9e94ccd0677f6d78a55
santro93 <nsantosh435@gmail.com>
2022-03-11 12:22:27 +0530

    Calcuated Wages for a Month

--- EmployeeWageComp.java
+++ EmployeeWageComp.java
@@ -6,20 +6,22 @@ public class EmployeeWageComp {
 		public static final int IS_PRESENT 			= 1;
 		public static final int IS_HALF_PRESENT 	= 2;
 		public static final int WAGE_PER_HR			= 20;
-		public static final int FULL_DAY_HR			= 8;
-		public static final int HALF_DAY_HR 		= 4;
-		public static final int NUM_OF_WORING_DAY 	= 20;
-		static int dailyWage 						= 0;
-		static int EmpHr							= 0;
-		static int totalEmpWage ;
-		public static double RANDOM;
+		public static final int NUM_OF_WORKING_DAY 	= 20;
+		public static final int MAX_WORKING_HR 	    = 100;
 		public static void main(String[] args) {
 								// Print Program Name
 		           System.out.println("\nWelcome to Employee Wage Computation Program");
-		          
-		          for (int days=0; days<NUM_OF_WORING_DAY; days++) {
-		        	   			RANDOM 		= Math.floor(Math.random()*3);
-		        	   			
+		         
+		        // Declared local variables        
+		         int DailyWage 	     = 0;
+		   		 int EmpHr     	     = 0;
+		   		 int TotalEmpWage    = 0;
+		   		 int TotalEmpHrs     = 0;
+		         int NoWorkingDay    = 0;
+		           
+		          while (TotalEmpHrs <= MAX_WORKING_HR && NoWorkingDay < NUM_OF_WORKING_DAY){
+		        	  			NoWorkingDay++;
+		        	  		double RANDOM = Math.floor(Math.random()*3);
 		           				// Employee Wage Calculation as per Present, Absent Or Half Time  By using Switch 
 		           switch((int)RANDOM){
 		           						
@@ -39,12 +41,14 @@ public class EmployeeWageComp {
 						
 		           }         
 							        // Calculate Daily Employee Wage
-							      	        dailyWage = WAGE_PER_HR * EmpHr;
-							      	        totalEmpWage += dailyWage;
-							      	   	   System.out.println("Employee Wage: " + dailyWage);
+							      	        DailyWage = WAGE_PER_HR * EmpHr;
+							      	        TotalEmpWage += DailyWage;
+							      	        TotalEmpHrs  += EmpHr;
+							      	   	   System.out.println("Employee Wage: " + DailyWage);
 		           
 		          }	
-		          		System.out.println("Total Employee Wage: " + totalEmpWage);
+		          		System.out.println("Total Employee Wage: " + TotalEmpWage);
+		          		System.out.println("Total Employee Working Days : " + TotalEmpHrs);
 	}
 
 }
